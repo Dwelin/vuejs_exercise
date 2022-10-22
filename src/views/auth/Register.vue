38ae8178fbf98051d0becb32c45289f0f7b5fb58
Dwelin <rehun@foxmail.com>
2022-10-22 17:57:56 +0800

    注册表单验证

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -6,10 +6,14 @@
           <h3 class="panel-title">请注册</h3>
         </div>
 
-        <div class="panel-body">
+        <div class="panel-body" data-validator-form>
           <div class="form-group">
             <label class="control-label">用户名</label>
             <input
+              v-validator:input.required="{
+                regex: /^[a-zA-Z]+\w*\s?\w*$/,
+                error: '用户名要求以字母开头的单词字符',
+              }"
               type="text"
               class="form-control"
               placeholder="请填写用户名"
@@ -18,6 +22,11 @@
           <div class="form-group">
             <label class="control-label">密码</label>
             <input
+              id="password"
+              v-validator.required="{
+                regex: /^\w{6,16}$/,
+                error: '密码要求 6 ~ 16 个单词字符',
+              }"
               type="password"
               class="form-control"
               placeholder="请填写密码"
@@ -26,6 +35,7 @@
           <div class="form-group">
             <label class="control-label">确认密码</label>
             <input
+              v-validator.required="{ target: '#password' }"
               type="password"
               class="form-control"
               placeholder="请填写确认密码"
@@ -34,13 +44,18 @@
           <div class="form-group">
             <label class="control-label">图片验证码</label>
             <input
+              v-validator.required="{ title: '图片验证码' }"
               type="text"
               class="form-control"
               placeholder="请填写验证码"
             />
           </div>
-          <div class="thumbnail" title="点击图片重新获取验证码">
-            <div class="captcha"></div>
+          <div
+            class="thumbnail"
+            title="点击图片重新获取验证码"
+            @click="getCaptcha"
+          >
+            <div class="captcha vcenter" v-html="captchaTpl"></div>
           </div>
           <button type="submit" class="btn btn-lg btn-success btn-block">
             <i class="fa fa-btn fa-sign-in"></i> 注册
@@ -52,8 +67,26 @@
 </template>
 
 <script>
+import createCaptcha from "@/utils/createCaptcha";
+
 export default {
   name: "Register",
+  data() {
+    return {
+      captchaTpl: "", // 验证码模板
+    };
+  },
+  created() {
+    this.getCaptcha();
+  },
+  methods: {
+    getCaptcha() {
+      const { tpl, captcha } = createCaptcha();
+
+      this.captchaTpl = tpl;
+      this.localCaptcha = captcha;
+    },
+  },
 };
 </script>
 
@@ -66,5 +99,10 @@ export default {
 .thumbnail .captcha {
   height: 46px;
   background: #e1e6e8;
+}
+.captcha {
+  font-size: 24px;
+  font-weight: bold;
+  user-select: none;
 }
 </style>
