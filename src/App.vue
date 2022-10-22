8b5cab314453171d955a9b96f62a795366fd5f0b
Dwelin <rehun@foxmail.com>
2022-10-22 16:59:45 +0800

    添加注册页面

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="wrap">
     <TheHeader />
+    <div id="main-container" class="container main-container">
+      <router-view />
+    </div>
     <TheFooter />
   </div>
 </template>
 
 <script>
-import TheHeader from '@/components/layouts/TheHeader'
-import TheFooter from './components/layouts/TheFooter.vue';
+import TheHeader from "@/components/layouts/TheHeader";
+import TheFooter from "./components/layouts/TheFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TheHeader,
-    TheFooter
-  }
-}
+    TheFooter,
+  },
+};
 </script>
 
 <style lang="scss">
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,62 +1,84 @@
 <template>
-    <div class="navbar navbar-default topnav">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" @click="toggleNav">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
-                <a href="/" class="navbar-brand">
-                    <span class="title">{{ logo.title }}</span>
-                    <img :src="logo.src" alt="logo.title">
-                </a>
-            </div>
+        <router-link to="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" alt="logo.title" />
+        </router-link>
+      </div>
 
-            <div id="top-navbar-collapse"  :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
-                <ul class="nav navbar-nav">
-                    <li v-for="(item,index) in navList" :class="{ active: index == activeNavIndex }">
-                        <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
-                    </li>
-                </ul>
-            </div>
+      <div
+        id="top-navbar-collapse"
+        :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]"
+      >
+        <ul class="nav navbar-nav">
+          <li
+            v-for="(item, index) in navList"
+            :class="{ active: index == activeNavIndex }"
+          >
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+
+        <!-- 入口组件 -->
+        <div class="navbar-right">
+          <TheEntry />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+// 引入 TheEntry.vue 的默认值
+import TheEntry from "@/components/layouts/TheEntry";
+
 export default {
-    name: "TheHeader",
-    data() {
-        return {
-            logo: {
-                // src 的值是用反引号（`）标识的模板字符串，我们只需将变量放在 ${} 的大括号之中
-                src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png!/both/24x24`,
-                title: 'Learnku Vue.js'
-            },
-            navList: ["社区","头条","问答","教程"],
-            activeNavIndex:0,
-            showCollapsedNav:false
-        }
+  name: "TheHeader",
+  // 添加 components 选项，并注册 TheEntry
+  components: {
+    TheEntry,
+  },
+  data() {
+    return {
+      logo: {
+        // src 的值是用反引号（`）标识的模板字符串，我们只需将变量放在 ${} 的大括号之中
+        src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png!/both/24x24`,
+        title: "Learnku Vue.js",
+      },
+      navList: ["社区", "头条", "问答", "教程"],
+      activeNavIndex: 0,
+      showCollapsedNav: false,
+    };
+  },
+  beforeCreate() {
+    // 在当前实例上添加一个 uploadsUrl 属性
+    this.uploadsUrl = "https://cdn.learnku.com/uploads/";
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index;
     },
-    beforeCreate() {
-        // 在当前实例上添加一个 uploadsUrl 属性
-        this.uploadsUrl = "https://cdn.learnku.com/uploads/"
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav;
     },
-    methods: {
-        changeNavIndex(index) {
-            this.activeNavIndex = index
-        },
-        toggleNav() {
-            this.showCollapsedNav = !this.showCollapsedNav
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-.title { display: none;}
-.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+.title {
+  display: none;
+}
+.navbar-default .navbar-nav > .active > a {
+  background: rgba(0, 0, 0, 0.03);
+}
 </style>
