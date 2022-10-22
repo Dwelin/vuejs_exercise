2ae6289fe02480550851e7e82dcaa45346edf4f5
Dwelin <rehun@foxmail.com>
2022-10-22 15:04:48 +0800

    导航列表

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -2,10 +2,25 @@
     <div class="navbar navbar-default topnav">
         <div class="container">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" @click="toggleNav">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
                 <a href="/" class="navbar-brand">
                     <span class="title">{{ logo.title }}</span>
                     <img :src="logo.src" alt="logo.title">
                 </a>
+            </div>
+
+            <div id="top-navbar-collapse"  :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+                <ul class="nav navbar-nav">
+                    <li v-for="(item,index) in navList" :class="{ active: index == activeNavIndex }">
+                        <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -20,16 +35,28 @@ export default {
                 // src 的值是用反引号（`）标识的模板字符串，我们只需将变量放在 ${} 的大括号之中
                 src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png!/both/24x24`,
                 title: 'Learnku Vue.js'
-            }
+            },
+            navList: ["社区","头条","问答","教程"],
+            activeNavIndex:0,
+            showCollapsedNav:false
         }
     },
     beforeCreate() {
         // 在当前实例上添加一个 uploadsUrl 属性
         this.uploadsUrl = "https://cdn.learnku.com/uploads/"
     },
+    methods: {
+        changeNavIndex(index) {
+            this.activeNavIndex = index
+        },
+        toggleNav() {
+            this.showCollapsedNav = !this.showCollapsedNav
+        }
+    }
 }
 </script>
 
 <style scoped>
 .title { display: none;}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
